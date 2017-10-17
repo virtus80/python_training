ca943541192371e7a5802ff87d676c0936d9ca1d
virtus80 <virtus80@mail.ru>
2017-10-17 20:54:43 +0300

    Extract parameters from methods in TestAddContact

--- test_add_contact.py
+++ test_add_contact.py
@@ -17,9 +17,10 @@ class test_add_contact(unittest.TestCase):
     def test_test_add_contact(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, login="admin", password="secret")
         self.open_contacts_page(wd)
-        self.create_contact(wd)
+        self.create_contact(wd, firstname="Vasyly", lastname="Petrov", nickname="vasya26", company="Google",
+        address="Belgorod, Lenina str., 10", mobile_phone="8-052-709-16-22", email="vasya.petrov@gmail.com")
         self.logout(wd)
 
     def logout(self, wd):
@@ -29,54 +30,51 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").send_keys("\\undefined")
 
-    def create_contact(self, wd):
+    def create_contact(self, wd, firstname, lastname, nickname, company, address, mobile_phone, email):
         # filling firstname
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Vasyly")
+        wd.find_element_by_name("firstname").send_keys(firstname)
         # filling lastname
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Petrov")
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Petrov")
+        wd.find_element_by_name("lastname").send_keys(lastname)
         # filling nickname
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("vasya26")
+        wd.find_element_by_name("nickname").send_keys(nickname)
         # filling company
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("Google")
+        wd.find_element_by_name("company").send_keys(company)
         # filling address
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("Belgorod, Lenina str., 10")
+        wd.find_element_by_name("address").send_keys(address)
         # filling mobile phone
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("8-052-709-16-22")
+        wd.find_element_by_name("mobile").send_keys(mobile_phone)
         # filling email
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("vasya.petrov@gmail.com")
+        wd.find_element_by_name("email").send_keys(email)
         # submit contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
     def open_contacts_page(self, wd):
         wd.find_element_by_link_text("add new").click()
 
-    def login(self, wd):
+    def login(self, wd, login, password):
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").send_keys("\\undefined")
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(login)
         wd.find_element_by_id("LoginForm").click()
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def open_home_page(self, wd):
