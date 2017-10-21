755ca38d8027a81dd53eb468bbd5eef42d1991ed
virtus80 <virtus80@mail.ru>
2017-10-21 20:45:39 +0300

    Extracted SessionHelper class for session managing

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,14 +13,14 @@ def app(request):
 
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="group1", header="header1", footer="footer1"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
 
 
 
