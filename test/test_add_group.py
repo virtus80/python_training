0d9e1cb496478ac59f6cf237ac68ce51a8e99e45
virtus80 <virtus80@mail.ru>
2017-10-21 20:34:05 +0300

    Created packages for different zone of responsibility

---
+++ test/test_add_group.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from fixture.application import Application
+from model.group import Group
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="group1", header="header1", footer="footer1"))
+    app.logout()
+
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
+
+
+
