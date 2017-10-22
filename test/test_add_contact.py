262be85d34d75bf7d5ae83109c285a4465e2d775
virtus80 <virtus80@mail.ru>
2017-10-22 18:26:17 +0300

    Deleted fixture creating from TestAddContact

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,20 +1,11 @@
 # -*- coding: utf-8 -*-
-import pytest
-from fixture.application import Application
 from model.contact import Contact
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact.create(Contact(firstname="Vasyly", lastname="Petrov", nickname="vasya26", company="Google",
-                               address="Belgorod, Lenina str., 10", mobile_phone="8-052-709-16-22", email="vasya.petrov@gmail.com"))
+    app.contact.create(Contact(firstname="Oleg", lastname="Lesovoy", nickname="olegles", company="Google",
+                               address="Belgorod, Lenina str., 12", mobile_phone="8-052-706-15-22", email="oleg.lesovoy@gmail.com"))
     app.session.logout()
 
 
