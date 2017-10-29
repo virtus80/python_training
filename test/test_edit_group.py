d77cf136bb3d38030918c300a3eba7e7a70216fb
virtus80 <virtus80@mail.ru>
2017-10-29 11:58:52 +0200

    Added conditions in edit_group method depends on changing field
    - added tests for edit_group_name and edit_group_header

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,3 +5,13 @@ def test_edit_first_group(app):
     app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(name="group1(ed)", header="header1(ed)", footer="footer1(ed)"))
     app.session.logout()
+
+def test_edit_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(name="New group"))
+    app.session.logout()
+
+def test_edit_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(header="New header"))
+    app.session.logout()
