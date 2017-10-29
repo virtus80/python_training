b8b96fac6a537ec3d3011fd4f033c6003567c6cb
virtus80 <virtus80@mail.ru>
2017-10-29 13:18:43 +0200

    Added conditions when fixture created

--- conftest.py
+++ conftest.py
@@ -1,10 +1,22 @@
 from fixture.application import Application
 import pytest
 
-@pytest.fixture(scope="session")
-def app(request):
-    fixture = Application()
-    fixture.session.login(username="admin", password="secret")
+fixture = None
+
+@pytest.fixture
+def app():
+    global fixture
+    if fixture is None:
+        fixture = Application()
+        fixture.session.login(username="admin", password="secret")
+    else:
+        if not fixture.is_valid():
+            fixture = Application()
+            fixture.session.login(username="admin", password="secret")
+    return fixture
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
     def fin():
         fixture.session.logout()
         fixture.destroy()
