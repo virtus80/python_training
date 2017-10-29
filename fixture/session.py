76072e917f988a6a7f24eea370da44b51f80d7b5
virtus80 <virtus80@mail.ru>
2017-10-29 11:23:27 +0200

    Removed pointless actions from login and logout methods

--- fixture/session.py
+++ fixture/session.py
@@ -7,11 +7,8 @@ class SessionHelper:
         wd = self.app.wd
         self.app.open_home_page()
         wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
-        wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("user").click()
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
@@ -20,7 +17,3 @@ class SessionHelper:
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").send_keys("\\undefined")
